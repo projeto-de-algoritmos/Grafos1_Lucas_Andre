b0b833cbe32892702dd079b746cc6d55e2932039
Lucas Medeiros <medeirosrosalucas@gmail.com>
2022-06-27 20:40:07 -0300

    Adicionar componente Footer
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>

---
+++ jogo-n-damas/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <p>
+            <strong>Como jogar:</strong> Clique em um espaço vazio no tabuleiro para colocar uma dama nela. O jogo termina quando todas as damas são 
+            colocadas no tabuleiro e duas damas não compartilham a mesma linha, coluna ou diagonal.
+        </p>
+    </footer>
+);
+
+export default Footer;
